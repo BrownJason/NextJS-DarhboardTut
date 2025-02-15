cf4567f62bd451898d8232c2d1cf139168da6167
Jason <jbjbro6@gmail.com>
2025-02-14 22:41:50 -0500

    remove last change to auth.ts

--- auth.ts
+++ auth.ts
@@ -25,22 +25,18 @@ export const { auth, signIn, signOut } = NextAuth({
       async authorize(credentials) {
         const parsedCredentials = z.object({ email: z.string().email(), password: z.string().min(6) }).safeParse(credentials);
 
-        try {
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            if (passwordsMatch) return user;
-          }
-
-          console.log('Invalid credentials');
-        } catch (error) {
-          console.log(error);
-        } finally {
-          return null;
+          if (passwordsMatch) return user;
         }
+
+        console.log('Invalid credentials');
+
+        return null;
       },
     }),
   ],
