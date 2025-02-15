1e4990b79d0a04d965e223f8035432ba3d744724
Jason <jbjbro6@gmail.com>
2025-02-15 03:56:49 -0500

    remove animation from line chart for now.

--- app/ui/dashboard/LineChart.tsx
+++ app/ui/dashboard/LineChart.tsx
@@ -21,15 +21,6 @@ const LineChart = (props: { data: any[] }) => {
         borderColor: 'rgb(64, 0, 138)',
         borderWidth: 1,
         backgroundColor: ['rgba(255, 99, 132, 0.4)', 'rgba(255, 159, 64, 0.4)', 'rgba(255, 205, 86, 0.4)', 'rgba(75, 192, 192, 0.4)', 'rgba(54, 162, 235, 0.4)', 'rgba(153, 102, 255, 0.4)', 'rgba(41, 43, 207, 0.4)', 'rgba(54, 102, 205, 0.4)', 'rgba(153, 102, 15, 0.4)', 'rgba(241, 143, 207, 0.4)', 'rgba(8, 158, 41, 0.4)', 'rgba(199, 11, 36, 0.4)'],
-        animations: {
-          tension: {
-            duration: 1000,
-            easing: 'easeOutQuint',
-            from: 1,
-            to: 0,
-            loop: true,
-          },
-        },
       },
     ],
   };
